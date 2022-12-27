9bfc87110e4b46230a2ab33501570ef4c5b09ce5
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2022-12-27 11:03:15 -0300

    [M1S02] Ex 7 - Número primo

---
+++ Módulo 01/Semana 02/EX 07/index.js
@@ -0,0 +1,14 @@
+var primos = 0;
+var contador = 0;
+for (var i = 1; i <= 1000; i++) {
+  for (var t = 1; t <= i; t++) {
+    if (i % t == 0) {
+      contador++;
+    }
+  }
+  if (contador == 2) {
+    primos++;
+  }
+  contador = 0;
+}
+console.log('Entre 0 a 1000 existem ' + primos + ' números primos');
