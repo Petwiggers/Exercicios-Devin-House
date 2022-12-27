c5666fbed948b3192f7a575d78f39cb220b0e394
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2022-12-26 21:48:27 -0300

    [M1S02] Ex 5 - Pares e Impares

---
+++ Módulo 01/Semana 02/EX 05/index.js
@@ -0,0 +1,11 @@
+var numero = parseInt(prompt('Digite o número desejado:'));
+var par = 0,
+  impar = 0;
+for (var i = 0; i <= numero; i++) {
+  if (i % 2 == 0) {
+    par += 1;
+  } else if (i % 2 == 1) {
+    impar += 1;
+  }
+}
+alert('Entre 0 e ' + numero + ' tem ' + par + ' números pares e ' + impar + ' impares');
