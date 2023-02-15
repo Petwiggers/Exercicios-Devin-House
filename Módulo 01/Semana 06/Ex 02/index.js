4701c4a10db17621f4dc060f97cb2d45035a3afd
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2023-02-15 11:22:20 -0300

    [M1S06] Ex 2 - Timer

--- Módulo 01/Semana 06/Ex 02/index.js
+++ Módulo 01/Semana 06/Ex 02/index.js
@@ -1,4 +1,18 @@
-const funcaoHora = setInterval((horas,minutos,segundos)=> {
-    
-}) 
+const funcaoHora = () => {
+  const interval = setInterval(() => {
+    const dataAtual = new Date();
+    const dataFormatada = dataAtual.toLocaleTimeString('pt-br');
+    console.log(dataFormatada);
+  }, 1000);
 
+  setTimeout(() => {
+    clearInterval(interval);
+  }, 10000);
+};
+
+funcaoHora();
+
+// const data = new Date();
+// console.log(data);
+// const formatData = data.toLocaleTimeString();
+// console.log(formatData);
